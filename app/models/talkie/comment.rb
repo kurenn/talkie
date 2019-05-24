66d90e986c8d7994389435c8c4d0846850eb8f94
Abraham Kuri <kurenn@icalialabs.com>
2019-05-23 19:00:00 -0500

    Adds inverse_of option to comment association

--- app/models/talkie/comment.rb
+++ app/models/talkie/comment.rb
@@ -1,5 +1,7 @@
 module Talkie
   class Comment < ActiveRecord::Base
+    belongs_to :creator, polymorphic: true, inverse_of: :comments
+
     validates :body, presence: true
   end
 end
--- spec/models/talkie/comment_spec.rb
+++ spec/models/talkie/comment_spec.rb
@@ -6,5 +6,6 @@ module Talkie
   RSpec.describe Comment, type: :model do
     it { is_expected.to respond_to(:body) }
     it { is_expected.to validate_presence_of(:body) }
+    it { is_expected.to belong_to(:creator).inverse_of(:comments) }
   end
 end
