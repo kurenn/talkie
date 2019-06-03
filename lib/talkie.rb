fbc174985b0b75c58966c8223bd5a152e3fee0ab
Abraham Kuri <kurenn@icalialabs.com>
2019-06-03 10:17:35 -0500

    Adds optional syntax for configuration Talkie

--- lib/talkie.rb
+++ lib/talkie.rb
@@ -12,10 +12,10 @@ module Talkie
   @@comment_creator_handler = :email
   mattr_accessor :comment_creator_handler
 
-  @@creator_path = lambda { |user, router| "#" }
+  @@creator_path = lambda { |_user, _router| "#" }
   mattr_accessor :creator_path
 
-  @@creator_avatar_url = lambda { |user| "//api.adorable.io/avatars/40/abott@adorable.png" }
+  @@creator_avatar_url = lambda { |_user| "//api.adorable.io/avatars/40/abott@adorable.png" }
   mattr_accessor :creator_avatar_url
 
   def self.configure
