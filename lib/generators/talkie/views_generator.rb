afb7438f28c406f5d47ce34a5520f9c5e24c9e31
Abraham Kuri <kurenn@icalialabs.com>
2019-08-17 13:27:31 -0500

    Adds views installer for the comments

--- lib/generators/talkie/views_generator.rb
+++ lib/generators/talkie/views_generator.rb
@@ -5,10 +5,14 @@ require 'rails/generators/base'
 module Talkie
   module Generators
     class ViewsGenerator < Rails::Generators::Base
-      source_root File.expand_path('../../../../app/views/talkie', __FILE__)
+      source_root File.expand_path('../../../app/views/talkie', __dir__)
 
       def copy_views
-        directory "notifications_mailer", "app/views/talkie/notifications_mailer"
+        directory 'notifications_mailer', 'app/views/talkie/notifications_mailer'
+      end
+
+      def copy_comments_views
+        directory 'comments', 'app/views/talkie/comments'
       end
     end
   end
