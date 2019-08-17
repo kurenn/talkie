e2dacc3ce57f86a6c64ddf6113475afd3786e95a
Abraham <kurenn@icalialabs.com>
2019-08-17 13:33:40 -0500

    Adds views installer for the comments (#26)
    * Adds views installer for the comments
    
    * Fixes rubocop line too long

--- spec/generators/views_generator_spec.rb
+++ spec/generators/views_generator_spec.rb
@@ -1,15 +1,29 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-require "pathname"
-require "generators/talkie/views_generator"
+require 'rails_helper'
+require 'pathname'
+require 'generators/talkie/views_generator'
 
 RSpec.describe Talkie::Generators::ViewsGenerator, type: :generator do
-  describe "copy_views" do
-    it "is copied to the application" do
+  describe 'copy_comments_views' do
+    it 'copies the comments views to the application' do
       run_generator
-      mentiones_html_file = file "app/views/talkie/notifications_mailer/mentioned.html.erb"
-      mentiones_text_file = file "app/views/talkie/notifications_mailer/mentioned.text.erb"
+      comment_file = file 'app/views/talkie/comments/_comment.html.erb'
+      form_file = file 'app/views/talkie/comments/_form.html.erb'
+      template_file = file 'app/views/talkie/comments/_template.html.erb'
+
+      expect(comment_file).to exist
+      expect(form_file).to exist
+      expect(template_file).to exist
+      expect(Pathname.new('app/views/talkie/comments')).to be_directory
+    end
+  end
+
+  describe 'copy_views' do
+    it 'is copied to the application' do
+      run_generator
+      mentiones_html_file = file 'app/views/talkie/notifications_mailer/mentioned.html.erb'
+      mentiones_text_file = file 'app/views/talkie/notifications_mailer/mentioned.text.erb'
 
       expect(mentiones_html_file).to exist
       expect(mentiones_text_file).to have_correct_syntax
